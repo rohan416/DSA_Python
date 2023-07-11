5748ddb3baab2cb1ee539ff1878cca02557f9890
Rohan Chand <56724800+rohan416@users.noreply.github.com>
2023-07-11 22:32:50 +0530

    Update DLL_methods.py

--- DLL_methods.py
+++ DLL_methods.py
@@ -81,6 +81,23 @@ class DoublyLinkedList:
         self.length -=1
         
         return temp
+    
+    def get(self,index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        if index < self.length/2:
+            for _ in range(index):
+                temp = temp.next
+                
+        else:
+            temp =self.tail
+            for _ in range(self.length-1,index, -1):
+                trmp = temp.prev
+        return temp
+    
+        
+        
             
 
         
