a31dc274a84cd27a2c201eca41e3cebe54eba5a8
Rohan Chand <56724800+rohan416@users.noreply.github.com>
2023-07-11 22:48:39 +0530

    Update DLL_methods.py

--- DLL_methods.py
+++ DLL_methods.py
@@ -103,6 +103,30 @@ class DoublyLinkedList:
             return True
         return False
     
+    def insert(self,index, value):
+        if index < 0 or index > self.length:
+            return None
+        
+        elif index ==0:
+            return self.prepend(value)
+
+        elif index == self.length:
+            return self.append(value)
+        
+        new_node = Node(value)
+        before = self.get(index-1)
+        after = before.next
+        
+        new_node.prev = before
+        new_node.next = after
+        before.next = new_node
+        after.prev = new_node
+        
+        self.length += 1
+        return True
+        
+            
+             
     
         
         
