2b4021becc177c849e7a998b61aa7cd170762250
Rohan Chand <56724800+rohan416@users.noreply.github.com>
2023-07-16 19:31:35 +0530

    Enqueue & Dequeue using stack

--- Queue1.py
+++ Queue1.py
@@ -30,6 +30,13 @@ class MyQueue:
         
         while len(self.stack2) > 0:
             self.stack1.append(self.stack2.pop())
+    
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack1.pop()
+
 
     def peek(self):
         return self.stack1[-1]
@@ -50,5 +57,23 @@ q.enqueue(3)
 # Output the front of the queue
 print("Front of the queue:", q.peek())
 
+# Dequeue some values
+print("Dequeued value:", q.dequeue())
+print("Dequeued value:", q.dequeue())
+
+# Enqueue another value
+q.enqueue(4)
+
+# Output the front of the queue again
+print("Front of the queue:", q.peek())
+
+# Dequeue all remaining values
+print("Dequeued value:", q.dequeue())
+print("Dequeued value:", q.dequeue())
+
 # Check if the queue is empty
 print("Is the queue empty?", q.is_empty())
+
+# Dequeue from an empty queue and check if it returns None
+print("Dequeued value from empty queue:", q.dequeue())
+
