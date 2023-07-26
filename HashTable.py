d73c20c19f9e475837ee43bc323fb9b9172b93f3
Rohan Chand <56724800+rohan416@users.noreply.github.com>
2023-07-26 22:16:47 +0530

    Update HashTable.py

--- HashTable.py
+++ HashTable.py
@@ -18,12 +18,31 @@ class HashTable:
             self.data_map[index] = []
         self.data_map[index].append([key, value])
 
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
         
+    def keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
+
+                
           
             
 my_hash_table = HashTable()
 my_hash_table.set_item('bolts', 1400)
-my_hash_table.set_item('WAshers', 50)
+my_hash_table.set_item('Washers', 50)
+my_hash_table.set_item('lumber', 1300)
+print(my_hash_table.get_item('bolts'))
+print(my_hash_table.keys())
 
 my_hash_table.print_table()
             
