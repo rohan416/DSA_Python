b47b078b20777825a6f4716ff7bdfe74bb59b68a
Rohan Chand <56724800+rohan416@users.noreply.github.com>
2023-07-29 18:42:08 +0530

    Update HashTable1.py

--- HashTable1.py
+++ HashTable1.py
@@ -1,3 +1,10 @@
+'''Instructions:
+HT: Item In Common (⚡Interview Question)
+Write a function item_in_common(list1, list2) that takes two lists as input and returns True if there is at least one common item between the two lists, False otherwise.
+
+Use a dictionary to solve the problem that creates an O(n) time complexity.
+'''
+
 
 # METHOD 1 (Big 0 = O(n^2))
 
